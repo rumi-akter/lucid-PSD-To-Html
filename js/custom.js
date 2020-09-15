73cd3db6982dc595c22be2287f3b0d53f51d9897
rumi-akter <68759662+rumi-akter@users.noreply.github.com>
2020-09-15 10:45:59 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+
+  $( document ).ready(function() {
+    $('.single-item').slick({
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      autoplay: false,
+      autoplaySpeed: 1000,
+      dots: true,
+      arrows: false
+    });
+
+    
+    
+});
